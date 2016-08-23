93ac1573a5d172a64dfe033ac808565d7369b710
Cloud User <ec2-user@chef-client.pineya.com>
2016-08-23 16:58:10 -0400

    cleanup

--- deploy_war/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'deploy_war'
-license          'All rights reserved'
-description      'Deploy war to tomcat7'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-
-depends 'tomcat7'
