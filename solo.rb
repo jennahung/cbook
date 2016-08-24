c6ad47c69b9a164c8b5d2a99b7d4f962bc6d8bd9
jenna hung <jennahung@gmail.com>
2016-08-24 20:29:49 +0000

    modify

--- solo.rb
+++ solo.rb
@@ -1,4 +1,6 @@
 
-file_cache_path '/tmp/chef'
-cookbook_path '/home/centos/chef-repo/cookbooks'
+data_bag_path "/tmp/chef/data_bags"
+environment_path "/tmp/chef/environments"
+file_cache_path "/tmp/chef/cache"
+cookbook_path './cookbooks'
 
