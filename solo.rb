fd6b1ce00f76543f08ad0c397f476aa0d609dc2b
jenna hung <jennahung@gmail.com>
2016-08-25 23:20:33 +0000

    modify solo.rb

--- solo.rb
+++ solo.rb
@@ -1,6 +1,8 @@
 
-data_bag_path "/tmp/chef/data_bags"
-environment_path "/tmp/chef/environments"
+root = File.absolute_path(File.dirname(__FILE__))
+
 file_cache_path "/tmp/chef/cache"
-cookbook_path '/home/centos/chef-repo/cookbooks'
+cookbook_path root + '/cookbooks'
+data_bag_path root + '/data_bags'
+environment_path root + '/environments'
 
